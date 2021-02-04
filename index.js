16659623c1ce57a80391b75dcd2ef04ef12ddcb8
Seth Zimmerman <s.zim517@gmail.com>
2021-02-03 21:25:17 -0500

    Initial commit also core logic (whoops)

---
+++ index.js
@@ -0,0 +1,112 @@
+require('dotenv').config()
+
+const express = require('express');
+const axios = require('axios');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3210;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// THIS IS BAD CHANGE THIS
+let message_ts;
+let channel;
+app.post('/emojify', async (req, res) => {
+    const parsed = JSON.parse(req.body.payload);
+    if (parsed.type === 'message_action') {
+        message_ts = parsed.message_ts;
+        channel = parsed.channel.id;
+        const body = {
+            "trigger_id": parsed.trigger_id,
+            "view": {
+                "type": "modal",
+                "callback_id": "modal_cb_id",
+                "title": {
+                    "type": "plain_text",
+                    "text": "EMOJI-FY"
+                },
+                "submit": {
+                    "type": "plain_text",
+                    "text": "GO"
+                },
+                "blocks": [
+                    {
+                        "type": "input",
+                        "block_id": "emojification_input",
+                        "label": {
+                          "type": "plain_text",
+                          "text": "MAKE IT HAPPEN"
+                        },
+                        "element": {
+                          "type": "plain_text_input",
+                          "action_id": "plain_input",
+                          "placeholder": {
+                            "type": "plain_text",
+                            "text": "THY WILL BE DONE"
+                          }
+                        }
+                      }
+                ]
+              }
+          };
+          await axios.post('https://slack.com/api/views.open', body, {
+            headers: {
+                Authorization: `Bearer ${process.env.SLACK_AUTH_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+          });
+          
+      res.status(200).send();
+    } else if (parsed.type === 'view_submission') {
+        const values = parsed.view.state.values.emojification_input.plain_input.value;
+        const letters = values.toLowerCase().split('');
+        let isInvalid = false;
+        const numberOfLettersMapping = letters.reduce((total, currentLetter) => {
+            total[currentLetter]
+                ? total[currentLetter] += 1
+                : total[currentLetter] = 1;
+            if (total[currentLetter] > 2) {
+                isInvalid = true;
+            }
+            return total;
+        }, {});
+        if (isInvalid) {
+            const bod = {
+                errors: [
+                    {
+                        name: "input_validation",
+                        error: "Must use two or less of the same letter"
+                    }
+                ]
+            }
+            res.status(200).json(bod);
+            return;
+        }
+        const executeIfIncomplete = async (i = 0) => {
+            const body = {
+                channel: channel,
+                timestamp: message_ts
+            }
+            if (numberOfLettersMapping[letters[i]] === 2) {
+                body.name = `alphabet-yellow-${letters[i]}`;
+                numberOfLettersMapping[letters[i]] -= 1;
+            } else {
+                body.name = `alphabet-white-${letters[i]}`;
+            }
+            await axios.post('https://slack.com/api/reactions.add', body, {
+                headers: {
+                    Authorization: `Bearer ${process.env.SLACK_AUTH_TOKEN}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (letters.length - 1 > i) {
+                executeIfIncomplete(i + 1);
+            }
+        }
+        executeIfIncomplete();
+        res.status(200).send();
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
